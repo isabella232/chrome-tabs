a9d05eab22cad88872e4cae43de1b52077d22c35
Angelo Perera <angelo.perera@iress.com.au>
2015-08-24 14:25:04 +1000

    Basic knockout and typescript

---
+++ js/ko-chrome-tabs.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/knockout/knockout.d.ts" />
+module koChromeTabs {
+    ko.bindingHandlers["chromeTabs"] = {
+        init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+            
+        }
+    };
+
+    ko.components.register('chrome-tabs', {
+        viewModel: function (params) {            
+            //this.value = params.value;
+        },
+        template:
+        '<div class="chrome-tab">\
+             <div class="chrome-tab-favicon" > </div>\
+             <div class="chrome-tab-title" > </div>\
+             <div class="chrome-tab-close" > </div>\
+             <div class="chrome-tab-curves" >\
+             <div class="chrome-tab-curves-left-shadow"> </div>\
+             <div class="chrome-tab-curves-left-highlight" > </div>\
+             <div class="chrome-tab-curves-left" > </div>\
+             <div class="chrome-tab-curves-right-shadow" > </div>\
+             <div class="chrome-tab-curves-right-highlight" > </div>\
+             <div class="chrome-tab-curves-right" > </div>\
+             </div>\
+         </div>'
+    });
+}
